a0c575dd848bc17b347db321ad24d0d1033430d1
Margaret Le <margaretle1128@gmail.com>
2023-07-11 19:34:10 -0700

    style dashboard.html in teacher

--- src/main/resources/static/javascript/teacher/dashboard.js
+++ src/main/resources/static/javascript/teacher/dashboard.js
@@ -10,7 +10,7 @@ setContainerHeight();
 
 function toggleSection(section) {
     var main = document.getElementById('main');
-    var createClass = document.getElementById('createClass');
+    var archive = document.getElementById('archive');
     var listOne = document.getElementsByClassName('listOne')[0];
     var listTwo = document.getElementsByClassName('listTwo')[0];
     var listOneText = listOne.querySelector('.ul-text');
@@ -20,7 +20,7 @@ function toggleSection(section) {
   
     if (section === 'main') {
       main.style.display = 'block';
-      createClass.style.display = 'none';
+      archive.style.display = 'none';
   
       listOne.style.backgroundColor = 'white';
       listTwo.style.backgroundColor = 'transparent';
@@ -31,11 +31,11 @@ function toggleSection(section) {
       listOneText.style.fontWeight = '600';
       listTwoText.style.fontWeight = 'normal';
       imgOne.src = "/images/menu_alter.png";
-      imgTwo.src = "/images/add.png";
+      imgTwo.src = "/images/archive.png";
       
-    } else if (section === 'createClass') {
+    } else if (section === 'archive') {
       main.style.display = 'none';
-      createClass.style.display = 'block';
+      archive.style.display = 'block';
   
       listOne.style.backgroundColor = 'transparent';
       listTwo.style.backgroundColor = 'white';
@@ -46,7 +46,7 @@ function toggleSection(section) {
       listOneText.style.fontWeight = 'normal';
       listTwoText.style.fontWeight = '600';
       imgOne.src = "/images/menu.png";
-      imgTwo.src = "/images/add_alter.png";
+      imgTwo.src = "/images/archive_alter.png";
     }
 }
   
@@ -61,7 +61,38 @@ listOne.addEventListener('click', function() {
 });
   
 listTwo.addEventListener('click', function() {
-    toggleSection('createClass');
+    toggleSection('archive');
     listOne.style.backgroundColor = 'transparent';
     listTwo.style.backgroundColor = 'white';
 });
+
+function randomPastelColor() {
+  var hue = Math.floor(Math.random() * 360);
+  var saturation = 25 + Math.floor(Math.random() * 50);
+  var lightness = 75 + Math.floor(Math.random() * 10);
+  return 'hsl(' + hue + ', ' + saturation + '%, ' + lightness + '%)';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var rooms = document.getElementsByClassName('rooms');
+  for (var i = 0; i < rooms.length; i++) {
+      rooms[i].style.backgroundColor = randomPastelColor();
+  }
+});
+
+function toggleRoomContent(event) {
+  var roomContent = event.currentTarget.querySelector('.roomContent');
+  var options = event.currentTarget.querySelector('.options');
+  roomContent.classList.toggle('hidden');
+  options.classList.toggle('visible');
+}
+
+var rooms = document.querySelectorAll('.rooms');
+rooms.forEach(function(room) {
+  room.addEventListener('click', toggleRoomContent);
+});
+
+function toggleDropdown() {
+  var dropdownContent = document.getElementById("dropdownContent");
+  dropdownContent.style.display = (dropdownContent.style.display === "block") ? "none" : "block";
+}
