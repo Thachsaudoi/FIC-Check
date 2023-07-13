eca72aa295e2a192de721c606bbed2d650c4d7a6
Mark Vu <markvu2003@gmail.com>
2023-07-12 17:50:21 -0700

    changed database

--- src/main/java/com/ficcheck/ficcheck/config/WebSocketConfig.java
+++ src/main/java/com/ficcheck/ficcheck/config/WebSocketConfig.java
@@ -1,20 +1,20 @@
-package com.ficcheck.ficcheck.config;
+// package com.ficcheck.ficcheck.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-@Configurable
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("ws").withSockJS();
-    }
+// import org.springframework.beans.factory.annotation.Configurable;
+// import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+// import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+// import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+// import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+// @Configurable
+// @EnableWebSocketMessageBroker
+// public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+//     public void registerStompEndpoints(StompEndpointRegistry registry) {
+//         registry.addEndpoint("ws").withSockJS();
+//     }
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");
-    }
-}
+//     @Override
+//     public void configureMessageBroker(MessageBrokerRegistry registry) {
+//         registry.setApplicationDestinationPrefixes("/app");
+//         registry.enableSimpleBroker("/topic");
+//     }
+// }
