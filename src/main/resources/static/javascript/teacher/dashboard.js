07c936b6767dc609e13dba1c1b5c36a3c5f87c5c
Mark Vu <markvu2003@gmail.com>
2023-07-12 17:43:53 -0700

    added edit course + delete (delete unfinished)

--- src/main/resources/static/javascript/teacher/dashboard.js
+++ src/main/resources/static/javascript/teacher/dashboard.js
@@ -109,3 +109,68 @@ function closeForm() {
   document.body.style.overflow = "auto"; // Enable scrolling when form is closed
 }
 
+document.querySelectorAll("[id^='editForm-']").forEach(function(form) {
+  form.addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Retrieve the form values
+    let hashedCid = form.querySelector("#hashedCid").value;
+    let classroomName = form.querySelector("#classroomName").value;
+    let roomNumber = form.querySelector("#roomNumber").value;
+
+    console.log(classroomName);
+    console.log(roomNumber);
+    console.log(hashedCid);
+
+    if (confirm("Are you sure you want to make these changes?")) {
+        $.ajax({
+            type: 'POST',
+            url: '/teacher/edit/course',
+            data: {
+                hashedCid: hashedCid,
+                classroomName: classroomName,
+                roomNumber: roomNumber,
+            },
+            success: function(response) {
+                // Handle the successful response here
+                window.location.href="/teacher/dashboard";
+            },
+            error: function(xhr, status, error) {
+                // Handle any errors that occur during the request
+                console.error("An error occurred while updating the course:", error);
+            }
+        });
+    }
+  }
+)});
+
+
+function deleteCourse(hashedCid, classroomName, roomNumber) {
+  // Generate the confirmation message
+  let confirmationMessage = `To confirm, type "${classroomName}-${roomNumber}" in the box below:`;
+  console.log(classroomName);
+  console.log(roomNumber);
+  console.log(hashedCid);
+  // Show the prompt box to the user
+  let userInput = prompt(confirmationMessage);
+
+  if (userInput !== null && userInput.trim() === `${classroomName}-${roomNumber}`) {
+      // User confirmed the deletion, send request to the backend
+      $.ajax({
+          type: 'POST',
+          url: '/teacher/edit/deleteCourse',
+          data: {
+              hashedCid: hashedCid
+          },
+          success: function() {
+              window.location.href="/teacher/dashboard";
+          },
+          error: function(xhr, status, error) {
+              console.error("An error occurred while deleting the course:", error);
+          }
+      });
+  } else {
+      // User canceled the deletion or entered incorrect input
+      alert("Deletion canceled or invalid input.");
+  }
+}
