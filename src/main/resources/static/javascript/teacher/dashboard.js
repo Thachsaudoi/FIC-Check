e9dcd02b03d3f881da5fccb419991ac3e7104470
Margaret Le <margaretle1128@gmail.com>
2023-07-15 15:22:23 -0700

    responsive design: add options pop up

--- src/main/resources/static/javascript/teacher/dashboard.js
+++ src/main/resources/static/javascript/teacher/dashboard.js
@@ -1,221 +1,324 @@
+// Set container height
 function setContainerHeight() {
-    var container = document.getElementById('container');
-    var screenHeight = window.innerHeight;
-    var containerHeight = screenHeight - 40;
-    container.style.height = containerHeight + 'px';
+  var container = document.getElementById('container');
+  var screenHeight = window.innerHeight;
+  var containerHeight = screenHeight - 40;
+  container.style.height = containerHeight + 'px';
 }
 
 window.addEventListener('resize', setContainerHeight);
 setContainerHeight();
 
+// Handle changing each section in menu
 function toggleSection(section) {
-    var main = document.getElementById('main');
-    var archive = document.getElementById('archive');
-    var listOne = document.getElementsByClassName('listOne')[0];
-    var listTwo = document.getElementsByClassName('listTwo')[0];
-    var listOneText = listOne.querySelector('.ul-text');
-    var listTwoText = listTwo.querySelector('.ul-text');
-    var imgOne = document.getElementById('menu');
-    var imgTwo = document.getElementById('add');
+  var main = document.getElementById('main');
+  var archive = document.getElementById('archive');
+  var listOne = document.getElementsByClassName('listOne')[0];
+  var listTwo = document.getElementsByClassName('listTwo')[0];
+  var listOneText = listOne.querySelector('.ul-text');
+  var listTwoText = listTwo.querySelector('.ul-text');
+  var imgOne = document.getElementById('menu');
+  var imgTwo = document.getElementById('add');
 
-    var navOne = document.getElementsByClassName('navOne')[0];
-    var navTwo = document.getElementsByClassName('navTwo')[0];
+  var navOne = document.getElementsByClassName('navOne')[0];
+  var navTwo = document.getElementsByClassName('navTwo')[0];
 
-    if (section === 'main') {
-      main.style.display = 'block';
-      archive.style.display = 'none';
-  
-      listOne.style.backgroundColor = 'white';
-      listTwo.style.backgroundColor = 'transparent';
-      listTwo.classList.remove('listTwo--before-display', 'listTwo--after-display');
-      listOne.classList.remove('listOne--before-hide', 'listOne--after-hide');
-      listOneText.style.color = 'black';
-      listTwoText.style.color = 'white';
-      listOneText.style.fontWeight = '600';
-      listTwoText.style.fontWeight = 'normal';
-      imgOne.src = "/images/menu_alter.png";
-      imgTwo.src = "/images/archive.png";
-      navOne.style.color = 'black';
-      navTwo.style.color = 'white';
-      navOne.style.fontWeight = '600';
-      navTwo.style.fontWeight = 'normal';
-      
-    } else if (section === 'archive') {
-      main.style.display = 'none';
-      archive.style.display = 'block';
-  
-      listOne.style.backgroundColor = 'transparent';
-      listTwo.style.backgroundColor = 'white';
-      listTwo.classList.add('listTwo--before-display', 'listTwo--after-display');
-      listOne.classList.add('listOne--before-hide', 'listOne--after-hide');
-      listOneText.style.color = 'white';
-      listTwoText.style.color = 'black';
-      listOneText.style.fontWeight = 'normal';
-      listTwoText.style.fontWeight = '600';
-      imgOne.src = "/images/menu.png";
-      imgTwo.src = "/images/archive_alter.png";
-      navOne.style.color = 'white';
-      navTwo.style.color = 'black';
-      navOne.style.fontWeight = 'normal';
-      navTwo.style.fontWeight = '600';
-    }
-}
-  
-  
-var listOne = document.getElementsByClassName('listOne')[0];
-var listTwo = document.getElementsByClassName('listTwo')[0];
-  
-listOne.addEventListener('click', function() {
-    toggleSection('main');
+  if (section === 'main') {
+    main.style.display = 'block';
+    archive.style.display = 'none';
+
     listOne.style.backgroundColor = 'white';
     listTwo.style.backgroundColor = 'transparent';
-});
-  
-listTwo.addEventListener('click', function() {
-    toggleSection('archive');
+    listTwo.classList.remove('listTwo--before-display', 'listTwo--after-display');
+    listOne.classList.remove('listOne--before-hide', 'listOne--after-hide');
+    listOneText.style.color = 'black';
+    listTwoText.style.color = 'white';
+    listOneText.style.fontWeight = '600';
+    listTwoText.style.fontWeight = 'normal';
+    imgOne.src = "/images/menu_alter.png";
+    imgTwo.src = "/images/archive.png";
+    navOne.style.color = 'black';
+    navTwo.style.color = 'white';
+    navOne.style.fontWeight = '600';
+    navTwo.style.fontWeight = 'normal';
+    
+  } else if (section === 'archive') {
+    main.style.display = 'none';
+    archive.style.display = 'block';
+
     listOne.style.backgroundColor = 'transparent';
     listTwo.style.backgroundColor = 'white';
+    listTwo.classList.add('listTwo--before-display', 'listTwo--after-display');
+    listOne.classList.add('listOne--before-hide', 'listOne--after-hide');
+    listOneText.style.color = 'white';
+    listTwoText.style.color = 'black';
+    listOneText.style.fontWeight = 'normal';
+    listTwoText.style.fontWeight = '600';
+    imgOne.src = "/images/menu.png";
+    imgTwo.src = "/images/archive_alter.png";
+    navOne.style.color = 'white';
+    navTwo.style.color = 'black';
+    navOne.style.fontWeight = 'normal';
+    navTwo.style.fontWeight = '600';
+  }
+}
+
+
+var listOne = document.getElementsByClassName('listOne')[0];
+var listTwo = document.getElementsByClassName('listTwo')[0];
+
+listOne.addEventListener('click', function() {
+  toggleSection('main');
+  listOne.style.backgroundColor = 'white';
+  listTwo.style.backgroundColor = 'transparent';
+});
+
+listTwo.addEventListener('click', function() {
+  toggleSection('archive');
+  listOne.style.backgroundColor = 'transparent';
+  listTwo.style.backgroundColor = 'white';
 });
 
 var navOne = document.getElementsByClassName('navOne')[0];
 var navTwo = document.getElementsByClassName('navTwo')[0];
-  
+
 navOne.addEventListener('click', function() {
-    toggleSection('main');
-    navOne.style.backgroundColor = 'white';
-    navTwo.style.backgroundColor = 'transparent';
-});
-  
-navTwo.addEventListener('click', function() {
-    toggleSection('archive');
-    navOne.style.backgroundColor = 'transparent';
-    navTwo.style.backgroundColor = 'white';
+  toggleSection('main');
+  navOne.style.backgroundColor = 'white';
+  navTwo.style.backgroundColor = 'transparent';
 });
 
+navTwo.addEventListener('click', function() {
+  toggleSection('archive');
+  navOne.style.backgroundColor = 'transparent';
+  navTwo.style.backgroundColor = 'white';
+});
+
+// Random pastel color for each class
 function randomPastelColor() {
-  var hue = Math.floor(Math.random() * 360);
-  var saturation = 25 + Math.floor(Math.random() * 50);
-  var lightness = 75 + Math.floor(Math.random() * 10);
-  return 'hsl(' + hue + ', ' + saturation + '%, ' + lightness + '%)';
+var hue = Math.floor(Math.random() * 360);
+var saturation = 25 + Math.floor(Math.random() * 50);
+var lightness = 75 + Math.floor(Math.random() * 10);
+return 'hsl(' + hue + ', ' + saturation + '%, ' + lightness + '%)';
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  var rooms = document.getElementsByClassName('rooms');
-  for (var i = 0; i < rooms.length; i++) {
-      rooms[i].style.backgroundColor = randomPastelColor();
-  }
+var rooms = document.getElementsByClassName('rooms');
+for (var i = 0; i < rooms.length; i++) {
+    rooms[i].style.backgroundColor = randomPastelColor();
+}
 });
 
+// Flipping classes when width > 1200px
 function toggleRoomContent(event) {
-  var roomContent = event.currentTarget.querySelector('.roomContent');
-  var options = event.currentTarget.querySelector('.options');
-  roomContent.classList.toggle('hidden');
-  options.classList.toggle('visible');
+var roomContent = event.currentTarget.querySelector('.roomContent');
+var options = event.currentTarget.querySelector('.options');
+roomContent.classList.toggle('hidden');
+options.classList.toggle('visible');
 }
 
-
-
 var rooms = document.querySelectorAll('.rooms');
+
+function enableToggleRoomContent() {
 rooms.forEach(function(room) {
   room.addEventListener('click', toggleRoomContent);
 });
+}
 
+function disableToggleRoomContent() {
+rooms.forEach(function(room) {
+  room.removeEventListener('click', toggleRoomContent);
+});
+}
+
+if (window.innerWidth <= 1200) {
+disableToggleRoomContent();
+} else {
+enableToggleRoomContent();
+}
+
+window.addEventListener('resize', function() {
+if (window.innerWidth <= 1200) {
+  disableToggleRoomContent();
+} else {
+  enableToggleRoomContent();
+}
+});
+
+// Drop down profile menu
 function toggleDropdown() {
-  var dropdownContent = document.getElementById("dropdownContent");
-  dropdownContent.style.display = (dropdownContent.style.display === "block") ? "none" : "block";
+var dropdownContent = document.getElementById("dropdownContent");
+dropdownContent.style.display = (dropdownContent.style.display === "block") ? "none" : "block";
 }
 
 function navtoggleDropdown() {
-  var dropdownContent = document.getElementById("navdropdownContent");
-  dropdownContent.style.display = (dropdownContent.style.display === "block") ? "none" : "block";
+var dropdownContent = document.getElementById("navdropdownContent");
+dropdownContent.style.display = (dropdownContent.style.display === "block") ? "none" : "block";
 }
 
+// Open and close add class form
 function openForm() {
-  var formContainer = document.getElementById("formContainer");
-  formContainer.classList.add("visible");
-  document.body.style.overflow = "hidden"; // Prevent scrolling when form is open
+var formContainer = document.getElementById("formContainer");
+formContainer.classList.add("visible");
+document.body.style.overflow = "hidden"; // Prevent scrolling when form is open
 }
 
 function closeForm() {
-  var formContainer = document.getElementById("formContainer");
-  formContainer.classList.remove("visible");
-  document.body.style.overflow = "auto"; // Enable scrolling when form is closed
+var formContainer = document.getElementById("formContainer");
+formContainer.classList.remove("visible");
+document.body.style.overflow = "auto"; // Enable scrolling when form is closed
 }
 
+// Open and close edit form
 function openEdit() {
-  var formContainer = document.getElementById("editContainer");
-  formContainer.classList.add("visible");
-  document.body.style.overflow = "hidden"; // Prevent scrolling when form is open
+var formContainer = document.getElementById("editContainer");
+formContainer.classList.add("visible");
+document.body.style.overflow = "hidden"; // Prevent scrolling when form is open
 }
 
 function closeEdit() {
-  var formContainer = document.getElementById("editContainer");
-  formContainer.classList.remove("visible");
-  document.body.style.overflow = "auto"; // Enable scrolling when form is closed
+var formContainer = document.getElementById("editContainer");
+formContainer.classList.remove("visible");
+document.body.style.overflow = "auto"; // Enable scrolling when form is closed
 }
 
+// Edit form retrieving info
 document.querySelectorAll("[id^='editForm-']").forEach(function(form) {
-  form.addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent the default form submission
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent the default form submission
 
-    // Retrieve the form values
-    let hashedCid = form.querySelector("#hashedCid").value;
-    let classroomName = form.querySelector("#classroomName").value;
-    let roomNumber = form.querySelector("#roomNumber").value;
+  // Retrieve the form values
+  let hashedCid = form.querySelector("#hashedCid").value;
+  let classroomName = form.querySelector("#classroomName").value;
+  let roomNumber = form.querySelector("#roomNumber").value;
 
-    console.log(classroomName);
-    console.log(roomNumber);
-    console.log(hashedCid);
-
-    if (confirm("Are you sure you want to make these changes?")) {
-        $.ajax({
-            type: 'POST',
-            url: '/teacher/edit/course',
-            data: {
-                hashedCid: hashedCid,
-                classroomName: classroomName,
-                roomNumber: roomNumber,
-            },
-            success: function(response) {
-                // Handle the successful response here
-                window.location.href="/teacher/dashboard";
-            },
-            error: function(xhr, status, error) {
-                // Handle any errors that occur during the request
-                console.error("An error occurred while updating the course:", error);
-            }
-        });
-    }
-  }
-)});
-
-
-function deleteCourse(hashedCid, classroomName, roomNumber) {
-  // Generate the confirmation message
-  let confirmationMessage = `To confirm, type "${classroomName}-${roomNumber}" in the box below:`;
   console.log(classroomName);
   console.log(roomNumber);
   console.log(hashedCid);
-  // Show the prompt box to the user
-  let userInput = prompt(confirmationMessage);
 
-  if (userInput !== null && userInput.trim() === `${classroomName}-${roomNumber}`) {
-      // User confirmed the deletion, send request to the backend
+  if (confirm("Are you sure you want to make these changes?")) {
       $.ajax({
           type: 'POST',
-          url: '/teacher/edit/deleteCourse',
+          url: '/teacher/edit/course',
           data: {
-              hashedCid: hashedCid
+              hashedCid: hashedCid,
+              classroomName: classroomName,
+              roomNumber: roomNumber,
           },
-          success: function() {
+          success: function(response) {
+              // Handle the successful response here
               window.location.href="/teacher/dashboard";
           },
           error: function(xhr, status, error) {
-              console.error("An error occurred while deleting the course:", error);
+              // Handle any errors that occur during the request
+              console.error("An error occurred while updating the course:", error);
           }
       });
-  } else {
-      // User canceled the deletion or entered incorrect input
-      alert("Deletion canceled or invalid input.");
   }
+}
+)});
+
+// Delete class function
+function deleteCourse(hashedCid, classroomName, roomNumber) {
+// Generate the confirmation message
+let confirmationMessage = `To confirm, type "${classroomName}-${roomNumber}" in the box below:`;
+console.log(classroomName);
+console.log(roomNumber);
+console.log(hashedCid);
+// Show the prompt box to the user
+let userInput = prompt(confirmationMessage);
+
+if (userInput !== null && userInput.trim() === `${classroomName}-${roomNumber}`) {
+    // User confirmed the deletion, send request to the backend
+    $.ajax({
+        type: 'POST',
+        url: '/teacher/edit/deleteCourse',
+        data: {
+            hashedCid: hashedCid
+        },
+        success: function() {
+            window.location.href="/teacher/dashboard";
+        },
+        error: function(xhr, status, error) {
+            console.error("An error occurred while deleting the course:", error);
+        }
+    });
+} else {
+    // User canceled the deletion or entered incorrect input
+    alert("Deletion canceled or invalid input.");
+}
+}
+
+// Pop up options for each class when width <= 1200px
+// Function to toggle the responsive options for a specific .rooms element
+function toggleResponsiveOptions(event) {
+var room = event.currentTarget;
+var responsiveOptions = room.querySelector('.responsive-options');
+var backdrop = document.getElementById('backdrop');
+
+// Toggle the visibility of the responsive options
+responsiveOptions.classList.toggle('visible');
+
+// Toggle the visibility of the backdrop and apply the blur effect
+backdrop.classList.toggle('visible');
+
+// If the responsive options are visible, add a click event to the backdrop to close the popup
+if (responsiveOptions.classList.contains('visible')) {
+  backdrop.addEventListener('click', function closeResponsiveOptionsOnClick() {
+    closeResponsiveOptions(room);
+    backdrop.removeEventListener('click', closeResponsiveOptionsOnClick);
+  });
+}
+}
+
+// Function to close the responsive options for a specific .rooms element
+function closeResponsiveOptions(room) {
+var responsiveOptions = room.querySelector('.responsive-options');
+var backdrop = document.getElementById('backdrop');
+
+// Hide the responsive options
+responsiveOptions.classList.remove('visible');
+backdrop.classList.remove('visible');
+}
+
+// Function to enable toggleResponsiveOptions for each .rooms element
+function enableToggleResponsiveOptions() {
+rooms.forEach(function(room) {
+  room.addEventListener('click', toggleResponsiveOptions);
+});
+}
+
+// Function to disable toggleResponsiveOptions for each .rooms element
+function disableToggleResponsiveOptions() {
+rooms.forEach(function(room) {
+  room.removeEventListener('click', toggleResponsiveOptions);
+});
+}
+
+var rooms = document.querySelectorAll('.rooms');
+
+if (window.innerWidth <= 1200) {
+enableToggleResponsiveOptions();
+} else {
+disableToggleResponsiveOptions();
+closeAllResponsiveOptions(); // Close all popups if open when switching to >1200px
+}
+
+window.addEventListener('resize', function() {
+if (window.innerWidth <= 1200) {
+  enableToggleResponsiveOptions();
+} else {
+  disableToggleResponsiveOptions();
+  closeAllResponsiveOptions(); // Close all popups if open when switching to >1200px
+}
+});
+
+// Event listener for clicking on the backdrop to close the popup
+document.getElementById('backdrop').addEventListener('click', closeAllResponsiveOptions);
+
+// Close all responsive options and backdrop when switching to >1200px
+function closeAllResponsiveOptions() {
+rooms.forEach(function(room) {
+  closeResponsiveOptions(room);
+});
 }
