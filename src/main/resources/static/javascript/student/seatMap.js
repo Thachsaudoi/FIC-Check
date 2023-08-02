dabbf3e13f11aaeba7b0b6221250b2630d59a1cc
Paul <leangpaulkho@gmail.com>
2023-08-01 22:48:15 -0700

    reponsive seatmap

--- src/main/resources/static/javascript/student/seatMap.js
+++ src/main/resources/static/javascript/student/seatMap.js
@@ -85,9 +85,10 @@ async function studentGenerateSeatMap(data) {
     seatElement.setAttribute('data-seat-index', seatIndex);
 
     // Set the position of the seat based on the coordinates from the DEFAULT_SEATMAP
-    seatElement.style.position = 'absolute';
-    seatElement.style.left = `${seats[seatIndex].xCoordinate}px`;
-    seatElement.style.top = `${seats[seatIndex].yCoordinate}px`;
+        // Set the position of the seat based on the coordinates from the DEFAULT_SEATMAP
+        seatElement.style.position = 'absolute';
+        seatElement.style.left = `${seats[seatIndex].xPercentage}%`;
+        seatElement.style.top = `${seats[seatIndex].yPercentage}%`;
 
     seatElement.addEventListener('click', (event) => {
       const seatIndex = parseInt(event.target.getAttribute('data-seat-index'));
